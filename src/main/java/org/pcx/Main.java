56aa1087c63052dfa013e7354025e48f316cf507
angelillo1 <angalbgar2@alu.edu.gva.es>
2026-06-01 09:38:10 +0200

    Actu basica

--- src/main/java/org/pcx/Main.java
+++ src/main/java/org/pcx/Main.java
@@ -3,8 +3,36 @@ package org.pcx;
 import org.pcx.window.AppGrafica;
 import org.pcx.utilities.*;
 import org.pcx.discord.*;
+import java.util.Scanner;
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-            AppGrafica window = new AppGrafica();
+        //Cargo la aplicación gráfica
+
+        AppGrafica window = new AppGrafica();
+        //Importante, si se carga el proyecto con IntelliJ, cargará el proyecto desde la ruta PCX-PROJECT, lo cual hace que la ruta del token tiene que ser así.
+
+        //System.out.println(System.getProperty("user.dir"));
+
+        //Cargo el bot de discord.
+
+        /*
+        File f = new File("token.txt");
+        try(Scanner tokenreader = new Scanner(f))
+        {
+            if (tokenreader.hasNext()) {
+                String token = tokenreader.nextLine();
+                //System.out.println(token);
+                DiscordBuilder dc = new DiscordBuilder(token);
+            }
+            else
+            {
+                System.out.println("No se ha proporcionado un token");
+            }
+        }
+        catch(FileNotFoundException e){}
+        */
+
+
     }
 }
--- src/main/java/org/pcx/discord/DiscordBuilder.java
+++ src/main/java/org/pcx/discord/DiscordBuilder.java
@@ -1,6 +1,23 @@
 package org.pcx.discord;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.awt.*;
+import java.util.Scanner;
 public class DiscordBuilder {
+    public DiscordBuilder(String token)
+    {
+        JDA jda = JDABuilder.create(token,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.DIRECT_MESSAGES,
+                        GatewayIntent.MESSAGE_CONTENT
+                )
+                .build();
+        System.out.println("Bot encendido");
 
+    }
 }
